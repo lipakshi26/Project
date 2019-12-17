50b05526d3e76198c1b714b206edbc672b8ad9eb
lipakshi26 <56711971+lipakshi26@users.noreply.github.com>
2019-12-17 14:09:35 +0530

    Add files via upload

---
+++ Caseclass.scala
@@ -0,0 +1,19 @@
+case class data( name:String, id:Int)
+object Caseclass{
+	def main(args: Array[String]):Unit={
+	{ 
+		var c1= data("abc",101)
+		var c2= data("def",102)
+		var c3= data("ghi",103)
+		var c4= data("jkl",104)
+		var c5= data("mno",105)
+
+	    println("Name:  "+c1.name)
+	    println("Id:  "+c1.id)
+	    println("Name:  "+c2.name)
+	    println("Id:  "+c2.id)
+
+	    //+c1.name " Id " +c1.id ) 
+	}
+}
+}
